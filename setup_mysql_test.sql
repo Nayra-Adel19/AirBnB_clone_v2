f7e5548252183aaa0be4d4536ac33b1ae49d203f
Bassant-Adel <baadel404@gmail.com>
2024-01-17 14:34:16 -0800

    MySQL Setup Development :D

---
+++ setup_mysql_test.sql
@@ -0,0 +1,13 @@
+-- MySQL server
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- MYSQL DATABASE
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+GRANT ALL PRIVILEGES 
+ON `hbnb_test_db`.*
+TO 'hbnb_test'@'localhost';
+
+GRANT SELECT 
+ON `performance_schema`.*
+TO 'hbnb_test'@'localhost';
