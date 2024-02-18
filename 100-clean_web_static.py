b351270cbe8b995bc3772430d41079b6c166101e
Nayra-Adel19 <nayra.adel19@gmail.com>
2024-02-18 15:21:30 -0800

    dd

---
+++ 100-clean_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# including the most recent
+
+import os
+from fabric.api import *
+
+env.hosts = ['54.237.207.81', '52.207.61.136']
+
+
+def do_clean(number=0):
+    """ including the most recent """
+    number = 1 if int(number) == 0 else int(number)
+
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
